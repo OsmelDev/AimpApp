de522dad162635f79876baaa025b89cc4b65b2f9
Osmel <oprades4@gmail.com>
2025-08-10 22:59:21 -0400

    fix: renombrado la exportacion del componente de Button

--- FrontEnd/src/components/UI/Button.tsx
+++ FrontEnd/src/components/UI/Button.tsx
@@ -45,10 +45,10 @@ function Button({
   VariantProps<typeof buttonVariants> & {
     asChild?: boolean;
   }) {
-  const Comp = asChild ? Slot : "button";
+  const ButtonComponent = asChild ? Slot : "button";
 
   return (
-    <Comp
+    <ButtonComponent
       data-slot="button"
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
